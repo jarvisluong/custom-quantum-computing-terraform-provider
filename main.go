b788cd28a2c4a2b7c1d39afaddff3dd7e883be89
Jarvis Luong <21980965+jarvisluong@users.noreply.github.com>
2023-03-01 20:38:39 +0200

    add code for terraform plugin sdk v2

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"terraform-provider-hashicups/hashicups"
+	"terraform-provider-quantum-runners/quantum_runners"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
@@ -10,7 +10,7 @@ import (
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
-			return hashicups.Provider()
+			return quantum_runners.Provider()
 		},
 	})
 }
