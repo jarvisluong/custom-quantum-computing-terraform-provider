0e8e2f089b8b3938388784f6dc6ee3811043ca55
Jarvis Luong <21980965+jarvisluong@users.noreply.github.com>
2023-03-02 15:10:42 +0200

    add aws dependencies

---
+++ example/aws_dependencies.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_iam_service_linked_role" "braket" {
+  aws_service_name = "braket.amazonaws.com"
+}
+
+resource "aws_iam_role_policy_attachment" "braket_service_linked" {
+  role       = aws_iam_service_linked_role.braket.name
+  policy_arn = "arn:aws:iam::aws:policy/aws-service-role/AmazonBraketServiceRolePolicy"
+}
+
+resource "aws_iam_role" "braket_execution" {
+  name = "BraketExecutionRole"
+  path = "service-role"
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonBraketJobsExecutionPolicy"
+  ]
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "braket.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
