a4eb804b94fe8d9796515555da6afcbe61dbee2c
Jarvis Luong <21980965+jarvisluong@users.noreply.github.com>
2023-03-09 21:52:16 +0200

    get quantum task result

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,11 @@
 provider "quantumrunners" {
 }
 
+variable "output_key_prefix" {
+  type = string
+  default = "test"
+}
+
 data "quantumrunners_devices" "devices" {
 }
 
@@ -14,16 +19,27 @@ resource "quantumrunners_quantum_circuit" "circuit" {
 
 resource "quantumrunners_task" "task" {
   circuit = quantumrunners_quantum_circuit.circuit.action
-  device_id = "arn:aws:braket:us-west-1::device/qpu/rigetti/Aspen-M-3"
+  # device_id = "arn:aws:braket:us-west-1::device/qpu/rigetti/Aspen-M-3"
+  device_id = "arn:aws:braket:::device/quantum-simulator/amazon/sv1"
   output_destination = resource.aws_s3_bucket.braket_result.bucket
-  output_key_prefix = "test"
+  output_key_prefix = var.output_key_prefix
   shots = 10
 }
 
-output "devices" {
-  value = data.quantumrunners_devices.devices
+data "aws_s3_object" "run_result" {
+  count = quantumrunners_task.task.task_status == "COMPLETED" ? 1 : 0
+  bucket = resource.aws_s3_bucket.braket_result.bucket
+  key    = "${var.output_key_prefix}/${split("/", quantumrunners_task.task.task_id)[1]}/results.json"
 }
 
 output "task_id" {
-  value = quantumrunners_task.task.task_id
+  value = split("/", quantumrunners_task.task.task_id)[1]
+}
+
+output "task_status" {
+  value = quantumrunners_task.task.task_status
+}
+
+output "task_result" {
+  value = data.aws_s3_object.run_result[0].body
 }
