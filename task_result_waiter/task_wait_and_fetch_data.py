d70c7e78dc7c9b50d2a9ed5e1c75afee073e025b
Jarvis Luong <21980965+jarvisluong@users.noreply.github.com>
2023-03-12 16:24:56 +0200

    task wait and fetch data

--- task_result_waiter/task_wait_and_fetch_data.py
+++ task_result_waiter/task_wait_and_fetch_data.py
@@ -1,9 +1,13 @@
 import boto3
+from botocore.config import Config
 import sys
 import time
 
 # Let's use Amazon S3
-braket = boto3.resource('braket')
+braket_config = Config(
+    region_name = 'us-west-1'
+)
+braket = boto3.client('braket', config=braket_config)
 s3 = boto3.resource('s3')
 
 while True:
@@ -11,6 +15,6 @@ while True:
         quantumTaskArn=sys.argv[1]
     )
     if response['status'] == 'COMPLETED':
-        s3.meta.client.download_file(response['outputS3Bucket'], f"{response['outputS3Directory']}/results.json", f"results_{sys.argv[1]}.json")
+        s3.meta.client.download_file(response['outputS3Bucket'], f"{response['outputS3Directory']}/results.json", "out/results.json")
         break
     time.sleep(5.0)
