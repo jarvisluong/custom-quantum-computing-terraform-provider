90392209c98309a541edba507bc1a111b451b8cd
Jarvis Luong <21980965+jarvisluong@users.noreply.github.com>
2023-03-23 18:11:15 +0200

    add script to plot the result

--- task_result_waiter/task_wait_and_fetch_data.py
+++ task_result_waiter/task_wait_and_fetch_data.py
@@ -1,5 +1,8 @@
 import boto3
 from botocore.config import Config
+from qiskit.visualization import plot_histogram
+from pydash import count_by
+import json
 import sys
 import time
 
@@ -15,8 +18,15 @@ while True:
         quantumTaskArn=sys.argv[1]
     )
     if response['status'] == 'COMPLETED':
-        s3.meta.client.download_file(response['outputS3Bucket'], f"{response['outputS3Directory']}/results.json", "out/results.json")
+        target_download_file_path = "out/results.json"
+        s3.meta.client.download_file(response['outputS3Bucket'], f"{response['outputS3Directory']}/results.json", target_download_file_path)
         print("Downloaded to out/results.json")
+        print("Now plotting the result")
+        with open(target_download_file_path) as result_file:
+            content = json.load(result_file)
+            joined_measurement = map(lambda measurement: ''.join(map(lambda x: str(x), measurement)),content['measurements'])
+            counted_measurement = count_by(joined_measurement)
+            plot_histogram(counted_measurement, filename="out/result.jpg")
         break
     print(f"Task {sys.argv[1]} status is still {response['status']}")
     time.sleep(5.0)
